bf4b0f72bcf81f6aff8a2c32ae4b25cda386a2f8
wang <zxcvb2850@163.com>
2019-01-21 09:22:58 +0800

    合并Android代码

--- android/app/src/main/java/com/easysellapp/MainActivity.java
+++ android/app/src/main/java/com/easysellapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
-    protected void onCreate(Bundle saveInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);  // here
         super.onCreate(savedInstanceState);
     }
